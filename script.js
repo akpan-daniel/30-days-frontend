30230b6c75407955668a6970809b61747aa770dc
akpan-daniel <akpandaniel.n@gmail.com>
2021-12-01 00:04:41 +0100

    Summarized index page
    Decided to use bootstrap for index, as index is not part of assessment.

---
+++ script.js
@@ -0,0 +1,22 @@
+let obj;
+let content = document.getElementById("main");
+fetch("./challenges.json")
+  .then((res) => res.json())
+  .then((data) => (obj = data))
+  .then(() => {
+    for (let el of obj) {
+      let cont = `
+      <div class="col-md-6 col-lg-4">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="${el.image_url}" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">${el.title}</h5>
+          <p class="card-text">${el.title}</p>
+          <a href="#" class="btn btn-primary">Day ${el.day}</a>
+        </div>
+      </div>
+      </div>
+      `;
+      content.innerHTML += cont;
+    }
+  });
